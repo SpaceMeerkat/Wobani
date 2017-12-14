298998d756d3d18c4c31a6a70d9fe254c679c0f2
SpaceMeerkat <james.d.astro@gmail.com>
2017-12-14 18:11:31 +0000

    x output array shape fixed

--- GPTim.py
+++ GPTim.py
@@ -113,7 +113,7 @@ plt.savefig('GP example noise')
 
 ### Saving the output mean and standard deviation of the posterior ############
 
-output_data = np.transpose(np.vstack([x_prior,mean_function.flat,std]))
+output_data = np.transpose(np.vstack([x_prior.flat,mean_function.flat,std]))
 np.savetxt('Mean and 1 standard deviation GP posterior',output_data,delimiter=' ')
 
 ###############################################################################
